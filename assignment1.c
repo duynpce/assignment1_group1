bc83c0210a929db8bd15adb2ab5e210bc7b6306f
T316 <trongnp.work@gmail.com>
2025-06-18 22:00:18 +0700

    Update assignment1.c

--- assignment1.c
+++ assignment1.c
@@ -91,62 +91,76 @@ void add(){
 
 void edit(){
    int MustFindID;
-   int n=0,found=-1;
+   int StudentIndex=0,found=-1;
    student students[1000];
    printf("Enter Student's ID that you want to edit: ");
    scanf("%d", &MustFindID);
    FILE *f = fopen("student.txt","r");
-   while (fscanf(f,"%d %s %d %f",&students[n].ID, &students[n].FirstName, &students[n].LastName, &students[n].GPA) == 4)
+   while (fscanf(f,"%d|%99[^|]|%99[^|]|%f",&students[StudentIndex].ID, students[StudentIndex].FirstName, students[StudentIndex].LastName, &students[StudentIndex].GPA) == 4)
    {
-      n++;
-      if (students[n].ID==MustFindID)
+      if (students[StudentIndex].ID==MustFindID)
       {
-         found=n;
+         found=StudentIndex;
       }
+      StudentIndex++;
    }
    fclose(f);
    remove("student.txt");
-   if (found==-1) printf("Student with ID: %d does not exist",MustFindID);
-   else
+   if (found==-1)
    {
-      int type;
-      printf("Type a number choose information that you want to edit: ");
-      printf("1: First Name");
-      printf("2: Last Name");
-      printf("3: GPA");
+      printf("Student with ID: %d does not exist\n",MustFindID);
+      return;
+   }
+
+   int type=-1;
+   do
+   {
+      printf("Choose an option: \n");
+      printf("1: First Name\n");
+      printf("2: Last Name\n");
+      printf("3: GPA\n");
+      printf("0: Exit");
       scanf("%d",&type);
+      delete_buffer();
       switch (type)
       {
          case 1:
-         char NewFirstName[100];
-         printf("Type New First Name: ");
-         scanf("%s",NewFirstName);
-         strcpy(students[found].FirstName,NewFirstName);
-         break;
-
-         case 2:
-         char NewLastName[100];
-         printf("Type New Last Name: ");
-         scanf("%s",NewLastName);
-         strcpy(students[found].LastName,NewLastName);
-         break;
-
-         case 3:
-         int NewGPA;
-         printf("Type New GPA: ");
-         scanf("%d", &NewGPA);
-         students[found].GPA=NewGPA;
-         break;
-         defaut:
-         printf("Please type in range [1,3]\n");
+         {
+            char NewFirstName[100];
+            printf("Type New First Name: ");
+            scanf("%s",NewFirstName);
+            strcpy(students[found].FirstName,NewFirstName);
+            break;
          }
 
+         case 2:
+         {
+            char NewLastName[100];
+            printf("Type New Last Name: ");
+            scanf("%99[^\n]",NewLastName);
+            strcpy(students[found].LastName,NewLastName);
+            break;
+         }
+
+         case 3:
+         {
+            int NewGPA;
+            printf("Type New GPA: ");
+            scanf("%d", &NewGPA);
+            students[found].GPA=NewGPA;
+            break;
+         }
+         defaut:
+            printf("Please type a NUMBER in range [1,3]\n");
+         }
+      }
+      while (type);
+
          FILE *f=fopen("student.txt","w");
-         for (int i = 0; i < n; i++) {
-         fprintf(f,"%d %s %s %f\n", students[i].ID, students[i].FirstName, students[i].LastName, students[i].GPA);
+         for (int i = 0; i < StudentIndex; i++) {
+         fprintf(f,"%d|%s|%s|%.2f\n", students[i].ID, students[i].FirstName, students[i].LastName, students[i].GPA);
          fclose(f);
          printf("Edit successfully\n");
-   }
    }
 }
 
